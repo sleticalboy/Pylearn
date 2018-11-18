639427144c26a254313bc2331022a83577b38891
sleticalboy <sleticalboy@gmail.com>
2018-11-18 12:26:41 +0800

    number of python

---
+++ com/sleticalboy/python/basic/04_number.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- encoding=utf-8 -*-
+
+num = 2 + 3
+print(num)
+
+num = 2 * 3
+print(num)
+
+num = 2 / 3
+print(num)  # 0.6666666666666666
+
+num = 2 - 3
+print(num)  # -1
+
+num = 2 ** 3
+print(num)  # 8
+
+num = 2 ** (3 + 3)
+print(num)  # 64
+
+num = (2 + 3) ** 3
+print(num)  # 125
+
+num = 0.1 + 0.2
+print(num)  # 0.30000000000000004
+
+num = 0.1 * 3
+print(num)  # 0.30000000000000004
+
+age = 24  # type: int
+message = 'Happy ' + str(age) + 'th Birthday!'
+print(message)
