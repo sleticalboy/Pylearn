a73a23aa34bf1975e03bfbd728384a52f55a44ac
sleticalboy <sleticalboy@gmail.com>
2018-11-18 12:25:42 +0800

    variable of python

---
+++ com/sleticalboy/python/basic/02_variable.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- encoding=utf-8 -*-
+
+# python 命名规则：
+# 使用小写的变量名 下划线_
+
+message = 'Hello Python World'
+print(message)
+
+message = 'Hello Python Crash Cursor World'
+print(message)
